6773c19923c5ae1c7ac0940bf2365e6b8087da7c
Lala <lalagg@hotmail.com>
2020-04-16 00:47:59 -0400

    trying to make the styling better

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,11 +7,17 @@
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
 
+
+// Check if any of the options are selected
+// if none are tell the user they need at least one
+// else create the password
+//
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", "[", "]"];
 
+var bank = "abcdefABCD" // => "hello" => ["h","e","l","l","o"]
 console.log(numbers);
 console.log(lowercase);
 console.log(uppercase);
@@ -19,40 +25,39 @@ console.log(specialCharacters);
 
 function generatePassword() {
     var passwordLength = prompt("Choose a password length between 8 and 128 characters");
-    var password = '';
+    var password = "";
     var category = 0;
     if (passwordLength >= 8 && passwordLength <= 128) {
-        // var wantNumbers = confirm("Do you want it to contain numbers?");
-        // var wantLowerCase = confirm("Do you want to lowercase characters?");
-        //var wantSpecialCharacters = confirm("Do you want to special characters?");
-        //var wantUpperCase = confirm("Do you want to uppercase characters?");
+        var wantNumbers = confirm("Do you want it to contain numbers?");
+        var wantLowerCase = confirm("Do you want it to contain lowercase characters?");
+        var wantSpecialCharacters = confirm("Do you want it to contain special characters?");
+        var wantUpperCase = confirm("Do you want it to contain uppercase characters?");
 
-        // generate password
-        while (password.length < passwordLength) {
-            category = Math.floor(Math.random() * 4)
-            console.log(category)
-            if (category === 0 && wantNumbers) {
-                password += numbers[Math.floor(Math.random() * numbers.length)];
-            } else if (category === 1 && wantLowerCase) {
-                password += lowercase[Math.floor(Math.random() * lowercase.length)];
-            } else if (category === 2 && wantUpperCase) {
-                password += uppercase[Math.floor(Math.random() * uppercase.length)];
-            } else if (category === 3 && wantSpecialCharacters) {
-                password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+        if (wantNumbers || wantLowerCase || wantUpperCase || wantSpecialCharacters) {
+            // generate password
+            while (password.length < passwordLength) {
+                category = Math.floor(Math.random() * 4);
+                console.log(category);
+                if (category === 0 && wantNumbers) {
+                    password += numbers[Math.floor(Math.random() * numbers.length)];
+                } else if (category === 1 && wantLowerCase) {
+                    password += lowercase[Math.floor(Math.random() * lowercase.length)];
+                } else if (category === 2 && wantUpperCase) {
+                    password += uppercase[Math.floor(Math.random() * uppercase.length)];
+                } else if (category === 3 && wantSpecialCharacters) {
+                    password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+                }
             }
+
+            document.getElementById('generatedPassword').textContent = password;
+        } else {
+            alert('You must select at least one option')
         }
-
-    }
-
-    else {
+    } else {
         alert("Invalid Length")
     }
-
-
-
-    document.getElementById('generatedPassword').textContent = password;
-    var passwordBtn = document.getElementById('passwordButton');
-    passwordBtn.addEventListener('click', function () {
-        generatePassword()
-    })
 }
+var passwordBtn = document.getElementById('passwordButton');
+passwordBtn.addEventListener('click', function () {
+    generatePassword()
+})
